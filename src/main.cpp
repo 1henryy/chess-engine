d7b17d92e8ab7cfb84d3401406388332fa091653
Henry Shang <henryshang79@gmail.com>
2025-11-09 23:47:45 -0500

    define board coordinate system and piece enums

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using Bitboard = __UINT64_TYPE__;
+
+int main (int argc, char* argv[]){
+
+    std::cout << "Chess Engine" << "\n";
+    
+    Bitboard board = 0;
+    
+    return 0;
+}
